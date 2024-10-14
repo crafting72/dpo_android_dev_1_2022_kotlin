ea1699ee08427655c3cc62359db8c23c5d518651
crafting72 <matvey.revnivyh@gmail.com>
2024-10-15 03:26:01 +0500

    Homework is done. We have learned how to output data and write comments.

---
+++ 02_HelloKotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+package org.example
+
+fun main() {
+    println("My name is Matvey.") // Output + translation to a new line.
+    /*print("I can make chatbots in tg on aiogram (python3).")
+    commenting on the block.
+     */
+    print("I'm from Tyumen.") // Output
+}
