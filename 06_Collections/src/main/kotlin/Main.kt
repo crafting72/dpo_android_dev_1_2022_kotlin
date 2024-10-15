d33e1c89543aad7a75655575613620cbe79f4cb5
crafting72 <matvey.revnivyh@gmail.com>
2024-10-16 03:51:01 +0500

    Homework is done. We have learned the collections.

---
+++ 06_Collections/src/main/kotlin/Main.kt
@@ -0,0 +1,59 @@
+package org.example
+
+/*import kotlin.random.Random
+
+fun main() { //Part 1
+    println("Enter the number N (the number of numbers in the list), which is > 0.")
+    val number = readln().toIntOrNull()
+    while (true) {
+        if (number != null){
+            if (number <= 0) println("You entered a number <= 0, try again.")
+            else break
+        }
+        else println("You haven't entered anything, try again.")
+    }
+    var numbers = List<Int>(size = number!!.toInt()) { Random.nextInt(-5, 5 ) }
+   //println(numbers.joinToString(", ")) //The output is separated by a string and a separator.
+    println(numbers) //output via a list
+    numbers = numbers.toMutableList()
+    numbers.forEachIndexed { index, value -> if (value % 2 == 0) numbers[index] = value * 10 }
+    println(numbers.sumOf { it })
+    println(numbers.filter { it > 0 })
+}*/
+
+fun main() { //Часть 2
+    println("""Data preparation has begun:
+        |In order to finish entering new users, send an empty field, i.e. null.
+    """.trimMargin())
+    var i = 1
+    var login: String
+    var password: String
+    val table = hashMapOf<String, String>()
+    while (true) {
+        print("Fill in the login for $i the user: ")
+        login = readln()
+        if (login == "") break
+        print("Fill in the password for $i the user: ")
+        password = readln()
+        if (password == "") break
+        table[login] = password
+        i++
+    }
+    println("""Data preparation is finished!
+        |$table
+        |
+        |User authorization:
+    """.trimMargin())
+    while (true){
+        print("Enter your login: ")
+        login = readln()
+        print("Enter the password: ")
+        password = readln()
+        if (!table.keys.contains(login) || table[login] != password) println("Invalid login or password is specified\n")
+        else {
+            println("Welcome $login")
+            break
+        }
+    }
+    println(table)
+}
